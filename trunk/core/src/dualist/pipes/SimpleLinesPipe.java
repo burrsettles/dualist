081994cccea9ce8931056ce79ea3771eddfb97b7
burrsettles@gmail.com <burrsettles@gmail.com@f4b867f0-3abd-5902-72f6-30f9e5fa9219>
2011-08-30 21:31:57 +0000

    mild refactoring to separate learning from GUI

--- trunk/core/src/dualist/pipes/SimpleLinesPipe.java
+++ trunk/core/src/dualist/pipes/SimpleLinesPipe.java
@@ -1,4 +1,4 @@
-package guts.pipes;
+package dualist.pipes;
 
 
 import java.util.regex.Pattern;
@@ -12,23 +12,31 @@ import cc.mallet.pipe.Noop;
 import cc.mallet.pipe.Pipe;
 import cc.mallet.pipe.PrintInputAndTarget;
 import cc.mallet.pipe.SerialPipes;
-import cc.mallet.pipe.Target2Label;
 import cc.mallet.pipe.TokenSequence2FeatureSequence;
+import cc.mallet.pipe.TokenSequence2FeatureSequenceWithBigrams;
 import cc.mallet.pipe.TokenSequenceRemoveStopwords;
-import cc.mallet.types.AugmentableFeatureVector;
 import cc.mallet.types.Instance;
 import cc.mallet.util.CharSequenceLexer;
 
-public class EntityPipe extends Pipe {
+public class SimpleLinesPipe extends Pipe {
 
     private Pipe myPipe = new SerialPipes(new Pipe[] {
-            new VanillaPipe("\\|\\|"),
-            new OrthoPipe(),
+            new CharSequenceReplace(Pattern.compile("\\<.*?>"), ""),
+            new CopyData2Source(),
+//            new CharSequenceReplace(Pattern.compile("&(.*?);"), ""),
+//            new CharSequenceReplace(Pattern.compile("[0-9]+"), "00"),
+            new CharSequenceLowercase(),
+            new CharSequence2TokenSequence(CharSequenceLexer.LEX_WORD_CLASSES),
+            new TokenSequenceBiGrammer(),
+            new TokenSequenceRemoveStopwords(),
+            new TokenSequence2FeatureSequence(),
+            new FeatureSequence2AugmentableFeatureVector(),
             new Labelize(),
 //            new PrintInputAndTarget(),
     });
 
     public Instance pipe (Instance carrier) {
+        carrier.setSource(carrier.getData());
         return myPipe.pipe(carrier);
     }
 
