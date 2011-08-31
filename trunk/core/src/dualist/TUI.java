345cb556c94cc904fa46883695a47c15a8e8ed89
burrsettles@gmail.com <burrsettles@gmail.com@f4b867f0-3abd-5902-72f6-30f9e5fa9219>
2011-08-31 22:21:50 +0000

    updated standalone classifier

--- trunk/core/src/dualist/TUI.java
+++ trunk/core/src/dualist/TUI.java
@@ -16,10 +16,15 @@ import dualist.pipes.DocumentPipe;
 
 public class TUI {
     
-    private static DecimalFormat df = new DecimalFormat("0.000000");
+    private static DecimalFormat df = new DecimalFormat("0.00000");
 
     public static void main(String[] args) throws Exception {
+        
+        // require at least two arguments to proceed
+        if (args.length < 2)
+            System.exit(1);
 
+        System.err.println("~ Loading trained model...");
         ObjectInputStream ois = new ObjectInputStream(new FileInputStream(args[0]));
         NaiveBayes nbModel = (NaiveBayes) ois.readObject();
 
@@ -28,6 +33,7 @@ public class TUI {
 
         InstanceList ilist = new InstanceList (myPipe);
 
+        System.err.println("~ Classifying input instances...");
         for (int ii = 1; ii < args.length; ii++) {
             Object fakeLabel = nbModel.getLabelAlphabet().lookupLabel(0);
             String name = (new File(args[ii])).getName();
@@ -49,7 +55,7 @@ public class TUI {
             Labeling l = nbModel.classify(instance).getLabeling();
             System.out.print(instance.getName());
             for (int rank = 0; rank < l.numLocations(); rank++)
-                System.out.print("\t" + l.getLabelAtRank(rank) + ":" + df.format(l.getValueAtRank(rank)));
+                System.out.print("\t" + l.getLabelAtRank(rank) + "\t" + df.format(l.getValueAtRank(rank)));
             System.out.println();
         }
 
