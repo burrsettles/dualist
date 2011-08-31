eb5dbabfe7d72326d046128554d1735931f35728
burrsettles@gmail.com <burrsettles@gmail.com@f4b867f0-3abd-5902-72f6-30f9e5fa9219>
2011-08-31 18:06:26 +0000

    added standalone classifier with cmdline interface

---
+++ core/src/dualist/TUI.java
@@ -0,0 +1,59 @@
+package dualist;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.text.DecimalFormat;
+
+import org.apache.commons.io.IOUtils;
+
+import cc.mallet.classify.NaiveBayes;
+import cc.mallet.pipe.Pipe;
+import cc.mallet.types.Instance;
+import cc.mallet.types.InstanceList;
+import cc.mallet.types.Labeling;
+import dualist.pipes.DocumentPipe;
+
+public class TUI {
+    
+    private static DecimalFormat df = new DecimalFormat("0.000000");
+
+    public static void main(String[] args) throws Exception {
+
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(args[0]));
+        NaiveBayes nbModel = (NaiveBayes) ois.readObject();
+
+        Pipe myPipe = nbModel.getInstancePipe();
+        boolean docPipe = (myPipe instanceof DocumentPipe);
+
+        InstanceList ilist = new InstanceList (myPipe);
+
+        for (int ii = 1; ii < args.length; ii++) {
+            Object fakeLabel = nbModel.getLabelAlphabet().lookupLabel(0);
+            String name = (new File(args[ii])).getName();
+            String text = IOUtils.toString(new FileInputStream(args[ii]), "UTF-8").trim();
+            if (text.isEmpty())
+                continue;
+            // if we're doing whole documents, then load 'er up...
+            if (docPipe)
+                ilist.addThruPipe(new Instance(text, fakeLabel, name, null));
+            // otherwise, assume each line represents and instance...
+            else {
+                String[] inputs = text.split("\\n+");
+                for (int i = 0; i< inputs.length; i++)
+                    ilist.addThruPipe(new Instance(inputs[i], fakeLabel, name+":"+i, null));
+            }        
+        }
+        
+        for (Instance instance : ilist) {
+            Labeling l = nbModel.classify(instance).getLabeling();
+            System.out.print(instance.getName());
+            for (int rank = 0; rank < l.numLocations(); rank++)
+                System.out.print("\t" + l.getLabelAtRank(rank) + ":" + df.format(l.getValueAtRank(rank)));
+            System.out.println();
+        }
+
+
+    }
+
+}
---
+++ core/src/dualist/pipes/CopyData2Source.java
@@ -0,0 +1,14 @@
+package dualist.pipes;
+
+import cc.mallet.pipe.Pipe;
+import cc.mallet.types.Instance;
+
+public class CopyData2Source extends Pipe {
+    private static final long serialVersionUID = 1;
+
+    public Instance pipe (Instance carrier) {
+        carrier.setSource(carrier.getData());
+        return carrier;
+    }
+
+}
