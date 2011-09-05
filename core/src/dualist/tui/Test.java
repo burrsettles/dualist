3346f1d6c5b099ccf397cba41766af1fa2c91ca5
burrsettles@gmail.com <burrsettles@gmail.com@f4b867f0-3abd-5902-72f6-30f9e5fa9219>
2011-09-05 18:21:04 +0000

    updated documentation and cmdline scripts

--- core/src/dualist/tui/Test.java
+++ core/src/dualist/tui/Test.java
@@ -2,11 +2,19 @@ package dualist.tui;
 
 import java.io.FileInputStream;
 import java.io.ObjectInputStream;
+import java.text.DecimalFormat;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
 
 import cc.mallet.classify.NaiveBayes;
+import cc.mallet.classify.NaiveBayesTrainer;
+import cc.mallet.pipe.Pipe;
 import cc.mallet.types.InstanceList;
 
 public class Test {
+
+    private static DecimalFormat df = new DecimalFormat("0.0000");
 
     public static void main(String[] args) throws Exception {
 
@@ -14,19 +22,43 @@ public class Test {
         if (args.length != 2)
             System.exit(1);
 
+        // load the trained model
         System.err.println("~ Loading trained model...");
         ObjectInputStream ois = new ObjectInputStream(new FileInputStream(args[0]));
         NaiveBayes nbModel = (NaiveBayes) ois.readObject();
         ois.close();
 
-        InstanceList ilist = Util.readZipData(args[1], nbModel.getInstancePipe(), null); 
+        // read in the data set
+        Pipe myPipe = nbModel.getInstancePipe();
+        InstanceList ilist = Util.readZipData(args[1], myPipe, null); 
 
+        // writes out some data stats
         System.out.println("|data|=" + ilist.size() + 
                 " |features|=" + ilist.getDataAlphabet().size() + 
                 " |labels|=" + ilist.getTargetAlphabet().size());
 
-        System.out.println(nbModel.getAccuracy(ilist));
+        // compute 10-fold cross-validation accuracy
+        InstanceList.CrossValidationIterator iter = ilist.crossValidationIterator(10);
+        Set<Double> accuracies = new HashSet<Double>();
+        while (iter.hasNext()) {
+            InstanceList[] split = iter.nextSplit();
+            InstanceList trainData = split[0];
+            InstanceList evalData = split[1];
+
+            NaiveBayesTrainer nbTrainer = new NaiveBayesTrainer(myPipe);
+            NaiveBayes cvModel = nbTrainer.train(trainData);
+
+            accuracies.add(cvModel.getAccuracy(evalData));
+        }
         
+        // report results
+        System.out.println();
+        System.out.println("model accuracy:\t" + 
+                df.format(nbModel.getAccuracy(ilist)));
+        System.out.println("10-fold XV acc:\t" + 
+                df.format(Util.average(accuracies)) + " +/- " + 
+                df.format(Util.stdev(accuracies)));
+        System.out.println();
     }
 
 }
